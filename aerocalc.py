84552b7c8e784855b8f0c876f54eb8464aff04eb
biriyanii <albinjohnm8@gmail.com>
2025-11-03 21:56:14 -0600

    Update aerocalc.py

--- aerocalc.py
+++ aerocalc.py
@@ -91,7 +91,7 @@ st.markdown("""
             --osu-orange: #FF6600;
         }
         .stApp {
-            background-color: #E6782E;
+            background-color: #FDFBD4;
             color: white;
         }
         h1, h2, h3 {
@@ -128,3 +128,4 @@ if st.button("Calculate"):
         st.write("### Results:")
         for key, value in result.items():
             st.write(f"**{key}:** {value:.6f}")
+
